327c995f3c51e0d8ee28645aa4e9a263afd737fa
nonard <nvmanansala.student@ua.edu.ph>
2022-08-20 09:50:58 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm <b>Nonard</b>.  My favorite hobby is playing video games and basketball. My
+favorite food is chicken and my nickname is Nonie. I'm also a Computer Enginnering student at the University of the Assumption. You can contact me on <Link href="https://www.facebook.com/nonardusi">Facebook.</Link></p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
